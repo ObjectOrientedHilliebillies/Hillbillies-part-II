a893fa670573b6f98b80d16f32d52489e381e315
Jonasvantrap <jonas.vantrappen@hotmail.com>
2016-05-05 14:07:13 +0200

    Statement

---
+++ src/hillbillies/model/statements/MoveToStatement.java
@@ -0,0 +1,9 @@
+package hillbillies.model.statements;
+
+public class MoveToStatement extends ActivityStatement{
+
+	public MoveToStatement() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/hillbillies/model/statements/SequenceStatement.java
@@ -0,0 +1,9 @@
+package hillbillies.model.statements;
+
+public class SequenceStatement extends Statement{
+
+	public SequenceStatement() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
